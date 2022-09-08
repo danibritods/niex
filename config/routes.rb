04e3c14e2db96eda21d581d64c7425fbef7c5235
Daniel Brito <danielbrito@pq.uenf.br>
2022-09-07 21:34:55 -0300

    feat: add bootstrap js and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # GET /about
-  get "about", to: "about#index"
+  get "/about", to: "about#index"
+  get "/", to: "main#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
