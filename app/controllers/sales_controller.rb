9f437f9c0a4178866ae55069922e74b80fb4722f
Daniel Brito <danielbrito@pq.uenf.br>
2022-11-28 12:56:03 -0300

    working

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,45 +1,86 @@
 class SalesController < ApplicationController
-    def new
-        # sale = Sale.new
-        @sale_products = []
-        Sale.build_inventory
-        if session[:user_id]
-            @user = User.find(session[:user_id])
-            if params[:search_text]
-                @product = params["search_text"]
-                Sale.new_product(@product)
-                @sale_products = Sale.show_products
-            end
-        end   
-    end
-    def search 
-        @product = params#[:query] + "t"#Product.find(params[:query])
-    end
-    def clean
-        Sale.clean_products
+    def index
+        # @search = " "#params[:barcode_query]
+        # @sale_products = []
+        # if params[:barcode_query]
+        #     Sale.new_product(@product)
+        #     @sale_products = Sale.show_products
+        # end
     end
 
+    def create
+        #Sale.save
+        Sale.destroy
+        redirect_to sale_url
+    end
+
+    def new
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+            if not @total
+                Sale.build_inventory
+            end
+            @sale_products = Sale.show_products
+            @total = Sale.show_total
+
+        end
+    end
+
+    def search 
+        # @product = params["/sell"]["query"]
+        query = params["/sell"]["query"]
+        quantity, prod = treat_query(query)
+        Sale.new_product(quantity, prod)
+        redirect_to sale_url
+    end
+    def destroy
+        Sale.destroy
+        redirect_to sale_url
+    end
+    
+    private 
+     def treat_query(query)
+         split_query = query.split("*")
+             if split_query.count == 2
+                     quantity, prod = split_query
+                     return [quantity, prod]
+                 else
+                     return ["1", split_query[0]]
+             end
+    end
 end
 
 class Sale
-    @@products = []
-    @@products_count = 0
-    @@inventory = {}
+    # def self.initialize
+        @@products = []
+        @@products_count = 0
+        @@inventory = {}
+        @@sale_total = 0
+    # end
 
-   def self.new_product(prod)
-    @@products_count += 1
-    @@products.append(
-        {prod_count: @@products_count}.merge(get_product(prod))
-    )
+   def self.new_product(quantity, prod)
+        @@products_count += 1
+        prod_info = get_product(prod)
+        prod_total = prod_info[:prod_price].to_f * quantity.to_f
+        @@sale_total += prod_total
+    
+        @@products.append(
+            {prod_count: @@products_count, prod_quantity: quantity, prod_total: prod_total }.merge(prod_info)
+        )
    end
 
    def self.show_products
     @@products
    end
 
-   def self.clean_products
+   def self.show_total
+    @@sale_total
+   end
+
+   def self.destroy
     @@products = []
     @@products_count = 0
+    @@sale_total = 0
    end
 
    def self.get_product(prod_code) 
@@ -58,4 +99,5 @@ class Sale
 
     @@inventory = prod_list.map{|prod_code, prod_name, prod_price| [prod_code, {prod_code: prod_code, prod_name: prod_name, prod_price: prod_price}]}.to_h
    end
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
- get "sell", to: "sales#new"
- get "get_prod", to: "sales#search"
+ get "sale", to: "sales#new"
+ post "sale", to: "sales#search" 
+ delete "sale", to: "sales#destroy"
+ get "sell", to: "sales#create"
+
+ get "index", to: "sales#index"
 
 #  get "products", to: "products#index" 
 #  get "products/:id", to: "products#show"
